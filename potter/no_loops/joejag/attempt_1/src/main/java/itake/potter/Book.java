3c52c1586ad0fccb9671ff9df0b64b429cc40d7f
Joe Wright <joe@joejag.com>
2017-05-11 12:54:11 +0300

    Potter, no loops, joe wright, attempt 1

---
+++ potter/no_loops/joejag/attempt_1/src/main/java/itake/potter/Book.java
@@ -0,0 +1,9 @@
+package itake.potter;
+
+public class Book {
+    public static final String FIRST = "first book title";
+    public static final String SECOND = "second book title";
+    public static final String THIRD = "third book title";
+    public static final String FOURTH = "fourth book title";
+    public static final String FIFTH = "fifth book title";
+}
